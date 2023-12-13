4c3d57b7500a26f67d12d46cecefd29c2fd9020d
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-12 21:07:25 -0300

    feat: desenvolve a sintaxe do controller de user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    //
+    public function store(Request $request) {
+        try {
+            $data = $request->all();
+
+            $user = User::create($data);
+
+            $request->validate([
+                'name' => 'string|required|max: 255',
+                'email' => 'string|required|max:255|unique:users',
+                'date_birth' => 'date_format:YYYY-mm-dd|string|required|unique:users',
+                'cpf' => 'string|required|max:255|unique:users',
+                'password' => 'string|required|min:8|max:32',
+                'plan_id'=> 'integer|required'
+            ]);
+
+            return $user;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
