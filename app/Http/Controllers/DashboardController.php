4b3b3c467b17665ed5033f9d8609f9d0e087337f
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-14 18:58:32 -0300

    feat: implementa a contagem do registro de estudante, exercicios e plano atual do usuario

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exercise;
+use App\Models\Plan;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request) {
-       $student =  Student::where('user_id', $request->user()->id)->count();
+       $registered_student =  Student::where('user_id', $request->user()->id)->count();
+       $registered_exercises =  Exercise::where('user_id', $request->user()->id)->count();
+       $current_user_plan = Plan::where('user_id', $request->user()->id)->count();
     }
 }
