8ae67b174aa94edf00fd29e579a4cb1a4464ae77
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-14 18:52:42 -0300

    feat: faz o count da quantidade de estudantes cadastrados pelo usuario

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index(Request $request) {
+       $student =  Student::where('user_id', $request->user()->id)->count();
+    }
 }
