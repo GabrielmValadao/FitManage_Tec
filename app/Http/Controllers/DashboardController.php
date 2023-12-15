5f251d39c9d73f2a989723541952de27e8934ace
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-15 13:16:35 -0300

    update: formata o documento

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,21 +9,22 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request) {
-       $students =  Student::where('user_id', $request->user()->id)->count();
-       $exercises =  Exercise::where('user_id', $request->user()->id)->count();
-       $userPlan = Plan::where('user_id', $request->user()->plan->description);
-       $limitPlan = Plan::where('user_id', $request->user()->plan->limit);
+    public function index(Request $request)
+    {
+        $students =  Student::where('user_id', $request->user()->id)->count();
+        $exercises =  Exercise::where('user_id', $request->user()->id)->count();
+        $userPlan = Plan::where('user_id', $request->user()->plan->description);
+        $limitPlan = Plan::where('user_id', $request->user()->plan->limit);
 
-       $remainingStudents = max(0, $limitPlan - $students);
+        $remainingStudents = max(0, $limitPlan - $students);
 
-       $data = [
-        'registered_students' => $students,
-        'registered_exercises' => $exercises,
-        'current_user_plan' => $userPlan,
-        'registered_students' => $remainingStudents,
-       ];
+        $data = [
+            'registered_students' => $students,
+            'registered_exercises' => $exercises,
+            'current_user_plan' => $userPlan,
+            'registered_students' => $remainingStudents,
+        ];
 
-       return response('', 200)->json($data);
+        return response('', 200)->json($data);
     }
 }
