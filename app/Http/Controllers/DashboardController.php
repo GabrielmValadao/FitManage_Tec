ca816fc6eb485363d6af61b551e49117eecb5383
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-15 13:11:13 -0300

    feat: alterações na sintaxe da função do index, que vai retornar os dados de estudantes, exercicios, plano e limite de cadastro

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,8 +10,20 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(Request $request) {
-       $registered_student =  Student::where('user_id', $request->user()->id)->count();
-       $registered_exercises =  Exercise::where('user_id', $request->user()->id)->count();
-       $current_user_plan = Plan::where('user_id', $request->user()->id)->count();
+       $students =  Student::where('user_id', $request->user()->id)->count();
+       $exercises =  Exercise::where('user_id', $request->user()->id)->count();
+       $userPlan = Plan::where('user_id', $request->user()->plan->description);
+       $limitPlan = Plan::where('user_id', $request->user()->plan->limit);
+
+       $remainingStudents = max(0, $limitPlan - $students);
+
+       $data = [
+        'registered_students' => $students,
+        'registered_exercises' => $exercises,
+        'current_user_plan' => $userPlan,
+        'registered_students' => $remainingStudents,
+       ];
+
+       return response('', 200)->json($data);
     }
 }
