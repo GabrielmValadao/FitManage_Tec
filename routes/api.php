f179c0888c7e68d1979c022402e9ecb0f53ce128
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-16 16:42:50 -0300

    feat: ajusta rota para get

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index']);
 
     Route::post('exercises', [ExerciseController::class, 'store']);
-    Route::post('exercises', [ExerciseController::class, 'index']);
+    Route::get('exercises', [ExerciseController::class, 'index']);
 });
 
 // rota pública
