00a415c884c7b8267ba8b5d05b696faf913b629d
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-15 21:47:06 -0300

    feat: cria a função de cadastro do exercicio

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exercise;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ExerciseController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        try {
+            $data = $request->all();
+
+            $request->validate([
+                'description' => 'string|required|max:255'
+            ]);
+
+            $exercise = Exercise::create($data);
+
+            return $exercise;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
