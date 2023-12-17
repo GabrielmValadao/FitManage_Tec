9bb6f628099d3e2fb6289b0110756af16890f73d
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-17 16:25:36 -0300

    feat: desenvolve a sintaxe de cadastro de estudante com a validação dos dados e  limite de usuario a partir do plano

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,11 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class StudentController extends Controller
 {
     public function store(Request $request)
     {
+        try {
+            $data = $request->all();
+
+            $request->validate([
+                'name' => 'string|required|max:255',
+                'email' => 'string|required|max:255|unique:students',
+                'date_birth' => 'date_format:Y-m-d|required',
+                'cpf' => 'string|required|max:255|unique:students',
+                'contact' => 'string|required|max:20',
+                'cep' => 'string|nullable',
+                'street' => 'string|nullable',
+                'state' => 'string|nullable',
+                'neighborhood' => 'string|nullable',
+                'city' => 'string|nullable',
+                'number' => 'string|nullable',
+                'complement' => 'string|nullable',
+                'user_id' => 'integer|required'
+            ]);
+
+            $user = $request->user();
+            $plan = $user->plan;
+
+            $maxStudentsCreate = $plan->limit;
+            $numberStudents = $user->students()->count();
+
+            if ($numberStudents >= $maxStudentsCreate) {
+                return response()->error('Limite de cadastro de estudante atingido', Response::HTTP_FORBIDDEN);
+            }
+
+
+
+
+
+            $student = Student::create($data);
+            return $student;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
     }
 }
