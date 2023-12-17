f4868d4872c7b0edb101d712e7b502a79655f373
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-17 15:42:27 -0300

    feat: define os dados que serão criados na tabela de estudante

--- database/migrations/2023_12_17_174715_create_students_table.php
+++ database/migrations/2023_12_17_174715_create_students_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->string('email', 255)->unique();
+            $table->date('date_birth');
+            $table->string('cpf', 14)->unique();
+            $table->string('contact', 20)->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('city', 50)->nullable();
+            $table->string('neighborhood', 50)->nullable();
+            $table->string('number', 30)->nullable();
+            $table->string('street', 30)->nullable();
+            $table->string('state', 2)->nullable();
+            $table->string('cep', 20)->nullable();
+            $table->string('complement', 50)->nullable();
             $table->timestamps();
         });
     }
