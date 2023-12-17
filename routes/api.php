bce6f202009f7b0da9596b3cb4324ec5c4d2de94
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-17 19:40:44 -0300

    feat: ajusta middleware na rota de cadastro de estudante

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('exercises', [ExerciseController::class, 'index']);
     Route::delete('exercises/{id}', [ExerciseController::class, 'destroy']);
 
-    Route::post('students', [StudentController::class, 'store'])->middleware(StudentRegistrationLimitMiddleware::class);
+    Route::middleware(StudentRegistrationLimitMiddleware::class)->post('students', [StudentController::class, 'store']);
 });
 
 // rota pública
