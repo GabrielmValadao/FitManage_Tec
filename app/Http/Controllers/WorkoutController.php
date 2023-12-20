2aa17e37919a62c9c0d7a0a8aaed5a78f2ca64e4
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-19 22:59:54 -0300

    feat: inicia a função store para cadastrar treino

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class WorkoutController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $request->validate([]);
+    }
 }
